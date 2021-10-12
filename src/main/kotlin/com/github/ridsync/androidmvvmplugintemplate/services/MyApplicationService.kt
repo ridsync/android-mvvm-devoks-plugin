ccc23ef9538b84037f2b20682c6b47b85d8e353a
GitHub Action <action@github.com>
2021-10-12 04:30:06 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ridsync/androidmvvmplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ridsync.androidmvvmplugintemplate.services
+
+import com.github.ridsync.androidmvvmplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/ridsync/androidmvvmplugintemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ridsync/androidmvvmplugintemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ridsync.androidmvvmplugintemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ridsync.androidmvvmplugintemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
